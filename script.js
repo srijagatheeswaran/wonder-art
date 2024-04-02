66ce20f1281a64f60ccdad70894d2d8a0dc8ac4f
srijagatheeswaran <102983197+srijagatheeswaran@users.noreply.github.com>
2024-04-02 20:44:20 +0530

    Add files via upload

--- script.js
+++ script.js
@@ -1,6 +1,16 @@
-let menubar = document.getElementById('menu');
+
+//for menu bar
+
 let content = document.getElementById('content')
 
 function menu(){
     content.classList.toggle('show');
+}
+//for focus
+
+function focusView() {
+    document.getElementById("middle").scrollIntoView({
+        behavior: "smooth"
+    });
+
 }
